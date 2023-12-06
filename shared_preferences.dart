ccee18c19b474673bc7d27a2e88bdf918c1c5f71
syadinaafifah <102166424+syadinaafifah@users.noreply.github.com>
2023-12-06 14:03:02 +0700

    Delete shared_preferences.dart

--- shared_preferences.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-// Menyimpan data
-SharedPreferences prefs = await SharedPreferences.getInstance();
-prefs.setString('key', 'value');
-
-// Membaca data
-String? value = prefs.getString('key');
